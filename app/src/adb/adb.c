9f4f3682fecd8623d7f606b31becea45c78fddcd
Agun <37329998+aagun@users.noreply.github.com>
2024-03-31 22:09:19 +0700

    Update adb.c

--- app/src/adb/adb.c
+++ app/src/adb/adb.c
@@ -25,12 +25,12 @@
 #define SC_ADB_COMMAND(...) { sc_adb_get_executable(), __VA_ARGS__, NULL }
 
 
-static int ends_with(const char *str, const char *suffix) {
-    size_t str_len = strlen(str);
-    size_t suffix_len = strlen(suffix);
+// static int ends_with(const char *str, const char *suffix) {
+//     size_t str_len = strlen(str);
+//     size_t suffix_len = strlen(suffix);
 
-    return (str_len >= suffix_len) && (!memcmp(str + str_len - suffix_len, suffix, suffix_len));
-}
+//     return (str_len >= suffix_len) && (!memcmp(str + str_len - suffix_len, suffix, suffix_len));
+// }
 
 static const char *adb_executable;
 
