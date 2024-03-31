8a40d930dac056210f6e0f772fe1c5c936896f60
Agun <37329998+aagun@users.noreply.github.com>
2024-03-31 22:05:14 +0700

    Update install_release.sh

--- install_release.sh
+++ install_release.sh
@@ -9,9 +9,9 @@ echo -n "[scrcpy] Downloading prebuilt server..."
 wget "$PREBUILT_SERVER_URL" -qO scrcpy-server
 echo "Done"
 
-echo -n "[scrcpy] Verifying prebuilt server... "
-echo -n "$PREBUILT_SERVER_SHA256  scrcpy-server" | sha256sum --check
-echo " Done"
+echo "[scrcpy] Verifying prebuilt server..."
+echo "$PREBUILT_SERVER_SHA256  scrcpy-server" | sha256sum --check
+echo "Done"
 
 echo "[scrcpy] Building client..."
 meson setup "$BUILDDIR" --buildtype=release --strip -Db_lto=true \
@@ -22,26 +22,26 @@ echo "Done"
 
 rm -rf meson-* compile_commands.json build.ninja .hgignore .gitignore
 
-PLATFORM_TOOLS_LINUX_URL="https://dl.google.com/android/repository/platform-tools-latest-linux.zip"
-PLATFORM_TOOLS="platform-tools"
+#PLATFORM_TOOLS_LINUX_URL="https://dl.google.com/android/repository/platform-tools-latest-linux.zip"
+#PLATFORM_TOOLS="platform-tools"
+#
+#echo -n "\n[platform-tools] Downloading platform-tools... "
+#wget $PLATFORM_TOOLS_LINUX_URL -qO $PLATFORM_TOOLS.zip
+#echo "Done"
+#
+#echo -n "[platform-tools] Extracting file $PLATFORM_TOOLS/adb ... "
+#unzip -p $PLATFORM_TOOLS.zip $PLATFORM_TOOLS/adb > adb
+#echo "Done"
+#
+#echo -n "[platform-tools] Grant execute permission to adb file... "
+#chmod +x "$(pwd)/adb"
+#echo "Done"
+#
+#echo -n "[platform-tools] Removing $PLATFORM_TOOLS.zip... "
+#rm $PLATFORM_TOOLS.zip
+#echo "Done"
 
-echo -n "\n[platform-tools] Downloading platform-tools... "
-wget $PLATFORM_TOOLS_LINUX_URL -qO $PLATFORM_TOOLS.zip
-echo "Done"
-
-echo -n "[platform-tools] Extracting file $PLATFORM_TOOLS/adb ... "
-unzip -p $PLATFORM_TOOLS.zip $PLATFORM_TOOLS/adb > adb
-echo "Done"
-
-echo -n "[platform-tools] Grant execute permission to adb file... "
-chmod +x "$(pwd)/adb"
-echo "Done"
-
-echo -n "[platform-tools] Removing $PLATFORM_TOOLS.zip... "
-rm $PLATFORM_TOOLS.zip
-echo "Done"
-
-echo -n "[scrcpy] Copying run file into $BUILDDIR... "
+echo -n "[scrcpy] Copying run file into $BUILDDIR directory... "
 cd .. && cp ./run ./$BUILDDIR
 echo "Done"
 
